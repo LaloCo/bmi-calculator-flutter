6cde666a576bc2ed12342bb7dd60fb54fec0ca3f
Eduardo Rosas <lalorosas@outlook.com>
2020-05-12 20:36:02 -0500

    Solve #1

--- lib/main.dart
+++ lib/main.dart
@@ -32,8 +32,11 @@ class _InputPageState extends State<InputPage> {
       body: Center(
         child: Text('Body Text'),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+      floatingActionButton: Theme(
+        data: ThemeData(accentColor: Color(0xFFFA0065)),
+        child: FloatingActionButton(
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
